1de56d1408c4764bf3042fe28640d1dba0467516
lynhe <lynhedev@gmail.com>
2025-02-11 12:22:41 +0100

    feat(auth): integrate auth module
    - add AuthController and AuthService to AppModule
    - update package.json with class-validator and @nestjs/mapped-types dependencies

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { ProductController } from './product/product.controller';
-import {ProductService} from "./product/product.service";
+import { ProductService } from "./product/product.service";
+import { AuthController } from "./auth/auth.controller";
+import { AuthService } from "./auth/auth.service";
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductController],
-  providers: [AppService, ProductService, PrismaService],
+  controllers: [AppController, ProductController, AuthController],
+  providers: [AppService, ProductService, PrismaService, AuthService],
 })
 export class AppModule {}
